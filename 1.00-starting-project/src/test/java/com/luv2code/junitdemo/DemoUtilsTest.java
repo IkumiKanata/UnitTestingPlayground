5f0d0ec2f17753622108c1bdb2f210c99d61d0d0
KanataIkumi <i.kanata0531@gmail.com>
2024-06-30 11:03:17 +0900

    test

--- 1.00-starting-project/src/test/java/com/luv2code/junitdemo/DemoUtilsTest.java
+++ 1.00-starting-project/src/test/java/com/luv2code/junitdemo/DemoUtilsTest.java
@@ -1,4 +1,31 @@
 package com.luv2code.junitdemo;
 
-public class DemoUtilsTest {
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DemoUtilsTest {
+
+    @Test
+    void testAdd() {
+
+        //create an object of the class we want to test
+        DemoUtils demoUtils = new DemoUtils();
+
+        //call the method we want to test
+        assertEquals(7, demoUtils.add(5, 2), "5 + 2 should be 7");
+        assertNotEquals(1, demoUtils.add(5, 2));
+    }
+
+    @Test
+    void testNullAndNotNull() {
+        //create an object of the class we want to test
+        DemoUtils demoUtils = new DemoUtils();
+
+        //call the method we want to test
+        assertNull(demoUtils.checkNull(null),"Should be null");
+        assertNotNull(demoUtils.checkNull("Hello"),"Should not be null");
+    }
+
+
 }
