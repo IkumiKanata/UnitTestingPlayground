845026460daa18cf85d55dfc3c8388c3b527c68b
KanataIkumi <i.kanata0531@gmail.com>
2024-07-02 10:40:32 +0900

    add spring starter test

--- 2.00-starting-project/src/test/java/com/luv2code/test/ApplicationExampleTest.java
+++ 2.00-starting-project/src/test/java/com/luv2code/test/ApplicationExampleTest.java
@@ -1,13 +1,66 @@
 package com.luv2code.test;
 
 import com.luv2code.component.MvcTestingExampleApplication;
+import com.luv2code.component.models.CollegeStudent;
+import com.luv2code.component.models.StudentGrades;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 @SpringBootTest(classes = MvcTestingExampleApplication.class)
 class ApplicationExampleTest {
 
+    private int count;
+
+    @Value("${info.app.name}")
+    private String appName;
+
+    @Value("${info.app.version}")
+    private String appVersion;
+
+    @Value("${info.app.description}")
+    private String appDescription;
+
+    @Value("${info.school.name}")
+    private String schoolName;
+
+    @Autowired
+    CollegeStudent collegeStudent;
+
+    @Autowired
+    StudentGrades studentGrades;
+
+    @BeforeEach
+    public void setUp() {
+        count = 1;
+        System.out.println("Testing:" + appName + " which is " + appDescription + " Version:" + appVersion
+        + ".Execution of test method " + count);
+
+        collegeStudent.setFirstname("John");
+        collegeStudent.setLastname("Doe");
+        collegeStudent.setEmailAddress("test.com");
+        studentGrades.setMathGradeResults(new ArrayList<>(List.of(90.0, 80.0, 70.0)));
+        collegeStudent.setStudentGrades(studentGrades);
+    }
+
     @Test
-    void contextLoads() {
+    @DisplayName("add grade results for student grades")
+    void AddGradeResultsForStudentGrades() {
+        assertEquals(240, studentGrades.addGradeResultsForSingleClass(collegeStudent.getStudentGrades().getMathGradeResults()));
+    }
+
+    @DisplayName("add grade results for student grades not equal")
+    @Test
+    void AddGradeResultsForStudentGradesNotEqual() {
+        assertNotEquals(250, studentGrades.addGradeResultsForSingleClass(collegeStudent.getStudentGrades().getMathGradeResults()));
     }
 }
