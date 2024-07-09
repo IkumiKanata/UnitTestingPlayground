9d703c0a057544f5a86816027581fa541b713f01
KanataIkumi <i.kanata0531@gmail.com>
2024-07-09 22:49:14 +0900

    add jdbctemplate

--- 3.00-starting-project/src/test/java/com/luv2code/springmvc/StudentAndGradeServiceTest.java
+++ 3.00-starting-project/src/test/java/com/luv2code/springmvc/StudentAndGradeServiceTest.java
@@ -8,7 +8,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.ApplicationContext;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.TestPropertySource;
 
